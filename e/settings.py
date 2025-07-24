4a369c3175834efe0f84b0857bdc5ee90db28c93
Lana_Qassas <lanaqassas823@gmail.com>
2025-07-24 12:25:36 +0300

    سهةحمثتصف

--- e/settings.py
+++ e/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "corsheaders",
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'core',
